706961fa9736d1b5e7531fbe2cfcb5c7f8f5b2f4
VytasKitm <vytautas.cerniauskas@stud.kitm.lt>
2024-12-15 23:52:42 +0200

    final_final

--- script.js
+++ script.js
@@ -14,75 +14,27 @@ const optionSelect2 = document.createElement("option")
 const selectHeader = document.createElement("h2")
 
 
+mainDiv.classList.add("mainDiv")
+inputDiv.classList.add("inputDiv")
+selectDiv.classList.add("selectDiv")
+buttonInput.classList.add("buttonInput")
 
-inputDiv.style.width = "300px"
 
-selectHeader.textContent = "Prideti miestai"
-
+mainDiv.setAttribute("id","mainDiv")
 input.setAttribute("type","text")
-input.setAttribute("placeholder","Iveskite miesto pavadinima")
-
-buttonInput.textContent = "Prideti miesta"
-
-labelSelect.textContent = "Pasirinkite prognozes data: "
-
+input.setAttribute("placeholder","Įveskite miesto pavadinimą")
+buttonInput.setAttribute("type","submit")
 inputSelect.setAttribute("name","date")
-
 optionSelect1.setAttribute("value","siandien")
 optionSelect2.setAttribute("value","rytoj")
 
-optionSelect1.textContent = "Siandien"
+
+optionSelect1.textContent = "Šiandien"
 optionSelect2.textContent = "Rytoj"
-
-
-
-
-body.style.background = "7280763.jpg"
-
-inputDiv.style.display = "flex"
-inputDiv.style.flexWrap = "wrap"
-inputDiv.style.justifyContent = "center"
-inputDiv.style.alignItems = "center"
-inputDiv.style.gap = "10px"
-inputDiv.style.marginBottom = "10px"
-
-input.style.height = "28px"
-
-buttonInput.setAttribute("type","submit")
-buttonInput.style.height = "30px"
-buttonInput.style.width = "100px"
-buttonInput.style.backgroundColor = "#4caf50"
-buttonInput.style.color = "white"
-buttonInput.style.border = "none"
-buttonInput.style.borderRadius = "3px"
-buttonInput.style.marginLeft = "10px"
-
-// inputSelect.style.margin = "10px 0px 20px 5px"
-
-rootDiv.style.display = "flex"
-rootDiv.style.flexDirection = "column"
-rootDiv.style.alignItems = "center"
-rootDiv.style.justifyContent = "start"
-rootDiv.style.width = "100%"
-rootDiv.style.height = "100vh"
-
-mainDiv.setAttribute("id","mainDiv")
-
-selectDiv.style.display = "flex"
-selectDiv.style.justifyContent = "center"
-selectDiv.style.flexWrap = "wrap"
-selectDiv.style.width = "80vw"
-selectDiv.style.gap = "20px"
-selectDiv.style.alignContent = "center"
-
-mainDiv.style.display = "flex"
-mainDiv.style.justifyContent = "center"
-mainDiv.style.flexWrap = "wrap"
-mainDiv.style.width = "700px"
-mainDiv.style.gap = "20px"
-mainDiv.style.alignContent = "start"
-
-mainHeader.textContent = "Oru prognozes"
+labelSelect.textContent = "Pasirinkite prognozės datą: "
+selectHeader.textContent = "Pridėti miestai"
+buttonInput.textContent = "Pridėti miestą"
+mainHeader.textContent = "Orų prognozės"
 
 
 const locations = [
@@ -117,11 +69,14 @@ const weatherStatus = {
         "null" : "./img/cloudy.png",
 }
 
+// city input value
 let selectValue
 
+//local storage
 const savedCities = localStorage.getItem("savedCities") == null ? localStorage.setItem("savedCities",JSON.stringify([])) : JSON.parse(localStorage.getItem("savedCities"))
 
 
+// adds all page elements except info cards
 function displayPage() {
         form.append(input)
         form.append(buttonInput)
@@ -135,11 +90,10 @@ function displayPage() {
         rootDiv.appendChild(mainDiv)
         rootDiv.appendChild(selectHeader)
         rootDiv.appendChild(selectDiv)
-
 }
 
-displayPage()
 
+// create info card and all its elements. sets id on main div and hidden is option to show delete button or not
 function createCard(id,hidden) {
         const infoMiestas = document.createElement("h4")
         const infoTemperature = document.createElement("span")
@@ -149,38 +103,28 @@ function createCard(id,hidden) {
         const cardDiv = document.createElement("div")
         const conditionImage = document.createElement("img")
         const conditionDiv = document.createElement("div")
-        conditionDiv.style.display = "flex"
-        conditionImage.style.width = "60px"
-        conditionImage.style.height = "60px"
-        conditionDiv.style.alignItems = "center"
-        conditionDiv.style.marginTop = "20px"
-        buttonDelete.setAttribute("id",`${id}`)
-        buttonDelete.addEventListener("click",deleteCards)
-        cardDiv.setAttribute("id",`city_${id}`)
+
+        cardDiv.classList.add("cardDiv")
+        conditionDiv.classList.add("conditionDiv")
+        conditionImage.classList.add("conditionImage")
+        buttonDelete.classList.add("buttonDelete")
         infoTemperature.classList.add("temp")
-        infoJutimine.classList.add("jutimine") 
-        cardDiv.style.position = "relative"
-        cardDiv.style.display = "flex"
-        cardDiv.style.flexDirection ="column"
-        cardDiv.style.alignItems = "center"
-        cardDiv.style.justifyContent = "start"
-        cardDiv.style.height = "200px"
-        cardDiv.style.width = "200px"
-        cardDiv.style.borderRadius = "10px"
-        cardDiv.style.backgroundColor = "aliceblue"
-        buttonDelete.style.width = "20px"
-        buttonDelete.style.height = "20px"
-        buttonDelete.style.padding = "0px"
-        buttonDelete.style.position = "absolute"
-        buttonDelete.style.top = "10px"
-        buttonDelete.style.right = "10px"
-        buttonDelete.style.borderRadius = "100%"
-        buttonDelete.style.border = "1px lightgrey solid"
+        infoJutimine.classList.add("jutimine")
+
+        buttonDelete.setAttribute("id",`${id}`)
+
+// adds event that deletes card to every button
+        buttonDelete.addEventListener("click",deleteCards)
+
+        cardDiv.setAttribute("id",`city_${id}`)
+         
+        infoOroSalygos.textContent = `Oro sąlygos: `
         buttonDelete.innerHTML = "&#10005;"
+// if hidden parameter is true hides delete card button
         if (hidden) {
                 buttonDelete.style.display = "none"
         }
-        infoOroSalygos.textContent = `Oro salygos: `
+        
         conditionDiv.append(infoOroSalygos,conditionImage)
         cardDiv.append(buttonDelete,infoMiestas,infoTemperature,infoJutimine,conditionDiv)
         
@@ -188,13 +132,8 @@ function createCard(id,hidden) {
 }
 
 
-form.addEventListener("submit", () => {
-        event.preventDefault()
-        addToLocalStorage()
-        drawSavedCards()
-})
 
-
+// create main cities cards
 function drawCards() {
         for(let city of locations) {
                 const card = createCard(`main_${city}`,true)
@@ -202,10 +141,12 @@ function drawCards() {
         }
 }
 
+// creates selected cities cards from local storage
 function drawSavedCards() {
         const arrayLS = JSON.parse(localStorage.getItem("savedCities"))
         for (selectCity of arrayLS) {
                 let target = document.getElementById(`city_${selectCity}`)
+// checks if that card already exists
                 if (!target) {
                        const card = createCard(`${selectCity}`,false)
                        selectDiv.append(card)  
@@ -213,9 +154,13 @@ function drawSavedCards() {
         }
 }
 
+
+// function that was added to every buttons event when creating info cards in createCard(). 
 function deleteCards() {
         const arrayLS = JSON.parse(localStorage.getItem("savedCities"))
+// gets id of the button that was pressed which is city name
         const deleteId = event.target.id
+// finds cardDiv with same id, removes it and that city from local storage
         const removeElement = document.getElementById(`city_${deleteId}`)
         const index = arrayLS.indexOf(`${deleteId}`)
         arrayLS.splice(index, 1)
@@ -223,46 +168,53 @@ function deleteCards() {
         removeElement.remove()
         }
 
-       
-inputSelect.addEventListener("change",() => {
-        updateWeather(createRegEx())
-        updateWeatherSelect(createRegEx())
-})
 
-
+// creates regular expresion of current time or next day 12.00
 function createRegEx() {
         const time = new Date()
         selectValue = inputSelect.value
         let timeRegEx
         if (selectValue == "siandien" || selectValue == undefined) {
                 timeRegEx = new RegExp(`${time.getFullYear()}-${(time.getMonth()+1).toString().padStart(2,'0')}-${time.getDate().toString().padStart(2,'0')} ${time.getHours().toString().padStart(2,'0')}:..:..`)    
-                console.log(`dabartinis regex= ${timeRegEx}`)
         }
-        else if (selectValue == "rytoj") {    
-                timeRegEx = new RegExp(`${time.getFullYear()}-${(time.getMonth()+1).toString().padStart(2,'0')}-${(time.getDate()+1).toString().padStart(2,'0')} 12:..:..`)    
-                console.log(`dabartinis regex= ${timeRegEx}`)
+        else if (selectValue == "rytoj") {
+// checks if next day is 32 then creates expresion of 01 day if true
+                const day1 = time.getDate()+1
+                const day2 = new Date(time.getFullYear(),time.getMonth(),day1)
+                if (day2.getDate() == 1) {
+                        timeRegEx = new RegExp(`${time.getFullYear()}-${(time.getMonth()+1).toString().padStart(2,'0')}-01 12:..:..`)    
+                }
+// adds +1 day 
+                else {
+                        timeRegEx = new RegExp(`${time.getFullYear()}-${(time.getMonth()+1).toString().padStart(2,'0')}-${(time.getDate()+1).toString().padStart(2,'0')} 12:..:..`)    
+                }  
         }
         return timeRegEx    
 }
 
 
+// checks if city from input exists in meteo data and if its already added in local storage
 async function addToLocalStorage() { 
         const arrayLS = JSON.parse(localStorage.getItem("savedCities"))
         const inputCity = input.value
         const res = await fetch(`https://api.meteo.lt/v1/places/${inputCity}/forecasts/long-term`)
         const data =  await res.json()
         try {
+// compares entered value with name returned from meteo api
                 if (data.place.name.toLowerCase() == inputCity.toLowerCase()) {
+// if above is true then checks if its already added to local storage
                         if (arrayLS.some(el => el == data.place.name)) {
-                                alert("Toks miestas jau pridetas")
+                                alert("Toks miestas jau pridėtas")
                         }
-                        else {
+                        else {  
+                                form.reset()
+                                input.focus()
                                 arrayLS.push(data.place.name)
                         }
                 }         
         }    
         catch(error){
-                alert("tokio miesto nera")
+                alert("Tokio miesto nėra")
         }                       
         localStorage.setItem("savedCities",JSON.stringify(arrayLS))
         drawSavedCards()
@@ -270,45 +222,47 @@ async function addToLocalStorage() {
 }
 
 
+// updates info card fields with information based on chosen time (timeRegEx)
 async function updateWeather(timeRegEx) {
         for(let city of locations) { 
                 const res = await fetch(`https://api.meteo.lt/v1/places/${city}/forecasts/long-term`)
                 const data =  await res.json()
-                const target = document.getElementById(`city_main_${city}`)
-                const cityName = target.querySelector("h4")
-                const temp = target.querySelector(".temp")
-                const jutimine = target.querySelector(".jutimine")
-                const conditionImage = target.querySelector("img")  
+// finds cardDiv with same id as the city fro array locations 
+                const parentDiv = document.getElementById(`city_main_${city}`)
+// gets all elements of this specific info card
+                const cityName = parentDiv.querySelector("h4")
+                const temp = parentDiv.querySelector(".temp")
+                const jutimine = parentDiv.querySelector(".jutimine")
+                const conditionImage = parentDiv.querySelector("img")  
                 for (let i in data.forecastTimestamps) {
+// finds entry with specific time and adds it to all elements
                         if (timeRegEx.test(data.forecastTimestamps[i].forecastTimeUtc)) {
                                 cityName.textContent = `${data.place.name}`
-                                temp.textContent = `Temperatura: ${data.forecastTimestamps[i].airTemperature}`
-                                jutimine.textContent = `Jutimine temperatura: ${data.forecastTimestamps[i].feelsLikeTemperature}`
+                                temp.innerHTML = `Temperatūra: <span class="tempInfo">${data.forecastTimestamps[i].airTemperature}</span>`
+                                jutimine.innerHTML = `Jutiminė temperatūra: <span class="tempInfo"> ${data.forecastTimestamps[i].feelsLikeTemperature}</span>`
                                 conditionImage.src = `${weatherStatus[data.forecastTimestamps[i].conditionCode]}` 
                                 break
                         }
                 }
         }
-
 }
 
-
+// updates selected cities info card. The same as above except cities is taken from local storage
 async function updateWeatherSelect(timeRegEx) {
         const arrayLS = JSON.parse(localStorage.getItem("savedCities"))
         for(selectCity of arrayLS) { 
-                console.log(selectCity)
                 const res = await fetch(`https://api.meteo.lt/v1/places/${selectCity}/forecasts/long-term`)
                 const data =  await res.json()
-                const target = document.getElementById(`city_${selectCity}`)
-                const city = target.querySelector("h4")
-                const temp = target.querySelector(".temp")
-                const jutimine = target.querySelector(".jutimine")
-                const conditionImage = target.querySelector("img")  
+                const parentDiv = document.getElementById(`city_${selectCity}`)
+                const city = parentDiv.querySelector("h4")
+                const temp = parentDiv.querySelector(".temp")
+                const jutimine = parentDiv.querySelector(".jutimine")
+                const conditionImage = parentDiv.querySelector("img")  
                 for (let i in data.forecastTimestamps) {
                         if (timeRegEx.test(data.forecastTimestamps[i].forecastTimeUtc)) {
                                 city.textContent = `${data.place.name}`
-                                temp.textContent = `Temperatura: ${data.forecastTimestamps[i].airTemperature}`
-                                jutimine.textContent = `Jutimine temperatura: ${data.forecastTimestamps[i].feelsLikeTemperature}`
+                                temp.innerHTML = `Temperatūra: <span class="tempInfo">${data.forecastTimestamps[i].airTemperature}</span>`
+                                jutimine.innerHTML = `Jutiminė temperatūra: <span class="tempInfo">${data.forecastTimestamps[i].feelsLikeTemperature}</span>`
                                 conditionImage.src = `${weatherStatus[data.forecastTimestamps[i].conditionCode]}` 
                                 break
                         }
@@ -316,13 +270,27 @@ async function updateWeatherSelect(timeRegEx) {
         }
 }
 
+// event listener for adding city to favorites
+form.addEventListener("submit", () => {
+        event.preventDefault()
+        addToLocalStorage()
+        drawSavedCards()
+})
 
+// event listener for changing time of forecast.
+inputSelect.addEventListener("change",() => {
+        updateWeather(createRegEx())
+        updateWeatherSelect(createRegEx())
+})
+
+// page reload event. Adds all elements, draw all cards and updates all info.
 document.addEventListener('DOMContentLoaded', function() {
+        displayPage()
         drawCards()
         drawSavedCards()
         updateWeather(createRegEx())
         updateWeatherSelect(createRegEx())
-    });
+});
 
 
 
